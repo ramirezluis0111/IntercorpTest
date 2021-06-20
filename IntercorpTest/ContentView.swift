e2e57fd25b78425fea9537af717508cc37efc11d
LuisRamirez <ramirezluis0111@gmail.com>
2021-06-20 11:44:27 -0300

    Initial Commit

---
+++ IntercorpTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntercorpTest
+//
+//  Created by Ramirez Luis on 20/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntercorpTest/IntercorpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntercorpTestApp.swift
+//  IntercorpTest
+//
+//  Created by Ramirez Luis on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct IntercorpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
